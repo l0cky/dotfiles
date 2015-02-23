2c21f818991c9e6df1a8af3bfeac92d2411c6ac0
breki <berki.csaba.jozsef@gmail.com>
2015-02-23 23:12:26 +0100

    Add install.sh, and add an if test, that stow is install in your computer or not.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# You have installed stow or not?
+
+if [[ $(dpkg-query -l 'stow' | grep -o '^ii') -ne 'ii' ]]; then
+  printf "Stow is not installed in your computer.\nPlease install it first.\n"
+else
+  printf "Stow is installed in your computer.\n"
+fi
+
+exit 0
